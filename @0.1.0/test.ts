64523450e18f71f268b12e1660033cc8059b9458
Alan Meile <alan.meile@zuehlke.com>
2020-11-05 19:05:15 +0100

    refactor tests

--- @0.1.0/test.ts
+++ @0.1.0/test.ts
@@ -5,30 +5,37 @@ Deno.test({
   name: "compile templates with denote",
   fn(): void {
     assertEquals(
-      "<h1>{}</h1>",
       denote("<h1>{}</h1>", undefined),
+      "<h1>{}</h1>",
     );
     assertEquals(
-      "<h1>hello world!</h1>",
       denote("<h1>{msg}</h1>", { msg: "hello world!" }),
+      "<h1>hello world!</h1>",
     );
     assertEquals(
-      "<h1>{hello world!}</h1>",
       denote("<h1>{{msg}}</h1>", { msg: "hello world!" }),
+      "<h1>{hello world!}</h1>",
     );
     assertEquals(
-      "<h1>Hello Alan</h1><h2>How are you?</h2>",
       denote(
         "<h1>Hello {name}</h1><h2>How {verb} you?</h2>",
         { name: "Alan", verb: "are" },
       ),
+      "<h1>Hello Alan</h1><h2>How are you?</h2>",
     );
     assertEquals(
-      "<h1>Hello world!</h1>\n<h1>I like denote.</h1>",
       denote(
         "<h1 map:parts={part}>{part}</h1>",
         { parts: ["Hello world!", "I like denote."] },
       ),
+      "<h1>Hello world!</h1>\n<h1>I like denote.</h1>",
+    );
+    assertEquals(
+      denote(
+        "<div map:fruits={fruit}><h1>I ate {number} {fruit}.</h1></div>",
+        { fruits: ["apples", "bananas"], number: 5 },
+      ),
+      "<div><h1>I ate 5 apples.</h1></div>\n<div><h1>I ate 5 bananas.</h1></div>",
     );
   },
 });
