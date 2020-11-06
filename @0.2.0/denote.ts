3861fcfeec6d610ee7115381ff55ce36530f5be6
Alan Meile <alan.meile@zuehlke.com>
2020-11-06 17:19:23 +0100

    implement version 0.2.0

--- @0.2.0/denote.ts
+++ @0.2.0/denote.ts
@@ -1,6 +1,6 @@
-const mapTemplateRegExp = new RegExp(/<([a-z0-9]*\b).*map:.*>[\s\S]+?<\/\1>/g);
-const mapRegExp = new RegExp(/map:[^\}]*}/g);
-const valueRegExp = new RegExp(/{[^{\}]*\}/g);
+const mapTemplateRegExp = /<([a-z0-9-]*\b).*map:.*>[\s\S]+?<\/\1>/g;
+const mapRegExp = /map:[^\}]*}/g;
+const valueRegExp = /{[^{\}]*\}/g;
 
 export function denote(template: string, data: any): string {
   const dataOrEmptyObject = data || {};
@@ -14,19 +14,30 @@ function transformMaps(template: string, dataOrEmptyObject: any): string {
   const keys = Object.keys(dataOrEmptyObject);
 
   for (const [mapTemplate] of mapTemplates) {
-    const [iterableAndValueSign] = <any> mapTemplate.match(mapRegExp);
-    const [mapSignature] = <any> iterableAndValueSign.match(mapRegExp);
+    const [mapSignature] = <any> mapTemplate.match(mapRegExp);
     const iterable = mapSignature.replace("map:", "").replace("=", "").replace(
       valueRegExp,
       "",
     );
-    const [valueSign] = <any> mapSignature.match(valueRegExp);
+    const [valueSignWithBrackets] = <any> mapSignature.match(valueRegExp);
+    const valueSign = <string>valueSignWithBrackets.slice(1, -1).trim();
     if (keys.includes(iterable)) {
       const parts: string[] = [];
-      for (const v of dataOrEmptyObject[iterable]) {
+      for (const valueOrObject of dataOrEmptyObject[iterable]) {
         const templatePart = mapTemplate.replace(" " + mapSignature, "");
-        const transformedPart = templatePart.replace(valueSign, v);
-        parts.push(transformedPart);
+        if (typeof valueOrObject === 'object') {
+          let transformedPart = templatePart;
+          for (const propertyName of Object.keys(valueOrObject)) {
+            const objectWithPropertyRegExp = new RegExp(`{[\\s]*${valueSign}.${propertyName}[\\s]*}`, 'g');
+            transformedPart = transformedPart.replaceAll(objectWithPropertyRegExp, valueOrObject[propertyName]);
+          }
+          parts.push(transformedPart);
+        }
+        if (typeof valueOrObject === 'string') {
+          const valueSignRegExp = new RegExp(`{[\\s]*${valueSign}[\\s]*}`, 'g');
+          const transformedPart = templatePart.replaceAll(valueSignRegExp, valueOrObject);
+          parts.push(transformedPart);
+        }
       }
       template = template.replace(mapTemplate, parts.join("\n"));
     }
--- test.ts
+++ test.ts
@@ -1,5 +1,25 @@
 import { assertEquals } from "https://deno.land/std@0.76.0/testing/asserts.ts";
-import { denote } from "./@0.1.0/denote.ts";
+import { denote } from "./@0.2.0/denote.ts";
+
+const template =
+  `<billboard-item onclick="read('{article.title}');" map:articles={article}>
+  <small>{ article.date }</small>
+  <h2>{ article.title }</h2>
+  <summary>{ article.summary }</summary>
+</billboard-item>
+`;
+
+const expectedResult = `<billboard-item onclick="read('birthday');">
+  <small>23.04.1993</small>
+  <h2>birthday</h2>
+  <summary>my birthday</summary>
+</billboard-item>
+<billboard-item onclick="read('st. georges day');">
+  <small>23.04.1993</small>
+  <h2>st. georges day</h2>
+  <summary>a special day</summary>
+</billboard-item>
+`;
 
 Deno.test({
   name: "compile templates with denote",
@@ -37,5 +57,35 @@ Deno.test({
       ),
       "<div><h1>I ate 5 apples.</h1></div>\n<div><h1>I ate 5 bananas.</h1></div>",
     );
+    assertEquals(
+      denote(
+        template,
+        {
+          articles: [
+            { date: "23.04.1993", title: "birthday", summary: "my birthday" },
+            {
+              date: "23.04.1993",
+              title: "st. georges day",
+              summary: "a special day",
+            },
+          ],
+        },
+      ),
+      expectedResult,
+    );
   },
 });
+
+denote(
+  template,
+  {
+    articles: [
+      { date: "23.04.1993", title: "birthday", summary: "my birthday" },
+      {
+        date: "23.04.1993",
+        title: "st. georges day",
+        summary: "a special day",
+      },
+    ],
+  },
+);
